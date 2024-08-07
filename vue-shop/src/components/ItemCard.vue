725ae51ddce16ed797110c81e02b595d24158748
matusbenky <matus.benky@gmail.com>
2024-08-07 20:36:55 +0200

    chore: Update ItemCard component to include modal functionality

--- vue-shop/src/components/ItemCard.vue
+++ vue-shop/src/components/ItemCard.vue
@@ -1,13 +1,16 @@
 <template>
-    <div v-if="open" class="item-card">
-        <h1>{{ item.category }}</h1>
-        <img :src="item.image" :alt="item.name" />
-        <h1>{{ item.name }}</h1>
-        <h2>{{ item.price }} $</h2>
-        <p>{{ item.description }}</p>
+    <div v-if="open" class="item-card-modal">
+        <div class="modal-content">
+            <button @click="$emit('close')" class="close-button">X</button>
+            <h1>{{ item.category }}</h1>
+            <img :src="item.image" :alt="item.name" />
+            <h1>{{ item.name }}</h1>
+            <h2>{{ item.price }} $</h2>
+            <p>{{ item.description }}</p>
+        </div>
     </div>
-    <div v-else>
-        <img :src="item.image" :alt="item.name" />
+    <div v-else class="item-card">
+        <img :src="item.image" :alt="item.name" @click="$emit('open')" />
         <h1>{{ item.name }}</h1>
         <h2>{{ item.price }} $</h2>
     </div>
@@ -29,23 +32,63 @@ export default {
 </script>
 
 <style scoped>
-.item-card-opened {
-    border: 1px solid #ddd;
-    padding: 20px;
-    margin-bottom: 20px;
-    text-align: center;
-}
-
 .item-card {
     border: 1px solid #ddd;
     padding: 20px;
     margin-bottom: 20px;
     text-align: center;
+    cursor: pointer;
 }
 
 .item-card img {
     max-width: 100%;
     height: auto;
     margin-bottom: 10px;
+}
+
+/* Modal styles */
+.item-card-modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.8);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000;
+}
+
+.modal-content {
+    background: #474747;
+    padding: 20px;
+    border-radius: 8px;
+    text-align: center;
+    position: relative;
+    max-height: 80vh;
+    max-width: 80vw;
+}
+.modal-content img {
+    object-fit: cover;
+    width: 50%;
+    height: auto;
+    aspect-ratio: 1 / 1; /* This keeps the image square */
+    max-width: 100%;
+    max-height: 100%;
+}
+
+
+.close-button {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    background: red;
+    color: #fff;
+    border: none;
+    font-size: 18px;
+    cursor: pointer;
+    padding: 5px 10px;
+    border-radius: 50%;
 }
 </style>
--- vue-shop/src/components/ListOfStores.vue
+++ vue-shop/src/components/ListOfStores.vue
@@ -1,50 +1,76 @@
 <template>
-    <nav>
-        <div>
-            <SearchBar :stores="stores" :items="currentStore" @selectStore="setCurrentStore" />
-            <ul>
-                <li v-for="store in stores" :key="store.id">
-                    <a @click="setCurrentStore(store.name)" class="text-white">{{ store.name }}</a>
-                </li>
-            </ul>
-        </div>
-
-        <div>
-            <input type="text" v-model="query" @input="filterStores" placeholder="Search for an item..." class="search-input" />
+    <div>
+        <nav>
             <div>
-                <h1>Max Price</h1>
-                <input type="number" v-model.number="maxPrice" @change="filterStores" class="search-input" /> 
-            </div>
-            <div>
-                <h1>Min Price</h1>
-                <input type="number" v-model.number="minPrice" @change="filterStores" class="search-input" /> 
-            </div>
-            <!-- Dropdown to select category -->
-            <div>
-                <h1>Category</h1>
-                <select class="search-input" v-model="selectedCategory" @change="filterStores">
-                    <option value="">All Categories</option>
-                    <option v-for="category in uniqueCategories" :key="category" :value="category">{{ category }}</option>
-                </select>
+                <SearchBar :stores="stores" :items="currentStore" @selectStore="setCurrentStore" />
+                <!-- <ul>
+                    <li v-for="store in stores" :key="store.id">
+                        <a @click="setCurrentStore(store.name)" class="text-white">{{ store.name }}</a>
+                    </li>
+                </ul> -->
             </div>
 
-            <div v-if="filteredStores.length == 0">
-                <button @click="currentStore = null">X</button>
-                <div v-for="item in currentStore" :key="item.id">
-                    <ItemCard :item="item" />
+            <div v-if="show">
+                <input type="text" v-model="query" @input="setQuery(query)" placeholder="Search for an item..."
+                    class="search-input" />
+                <div>
+                    <h1>Max Price</h1>
+                    <input type="number" v-model="maxPrice" @change="setMaxPrice(maxPrice)" class="search-input" />
+                </div>
+                <div>
+                    <h1>Min Price</h1>
+                    <input type="number" v-model="minPrice" @change="setMinPrice(minPrice)" class="search-input" />
+                </div>
+                <div>
+                    <h1>Category</h1>
+                    <div class="category-container">
+                        <div v-for="category in uniqueCategoriesWithImages" :key="category.name" class="category-item">
+                            <div v-if="!currentFilters.category.includes(category.name)"
+                                @click="setCategory(category.name)">
+                                <img :src="category.image" :alt="category.name" />
+                                <p>{{ category.name }}</p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <button @click="filterStores">Apply Filters</button>
+
+                <!-- Display selected filters -->
+                <div class="selected-filters">
+                    <span v-if="currentFilters.category.length > 0">
+                        <span v-for="name in currentFilters.category" :key="name">
+                            Category: {{ name }}
+                            <button @click="clearCategory(name)">X</button>
+                        </span>
+
+                    </span>
+                    <span v-if="currentFilters.minPrice !== null">
+                        Min Price: {{ minPrice }}
+                        <button @click="setMinPrice(null)">X</button>
+                    </span>
+                    <span v-if="currentFilters.maxPrice !== null">
+                        Max Price: {{ maxPrice }}
+                        <button @click="setMaxPrice(null)">X</button>
+                    </span>
+                    <span v-if="currentFilters.query !== ''">
+                        Query: {{ query }}
+                        <button @click="setQuery('')">X</button>
+                    </span>
+                </div>
+
+                <div>
+                    <button @click="currentStore = null">X</button>
+                    <div v-for="item in filteredStores" :key="item.id">
+                        <ItemCard :item="item" :open="item.id === openItemId" @open="openItem(item.id)"
+                            @close="closeItem" />
+                    </div>
                 </div>
             </div>
-            <div v-else>
-                <button @click="currentStore = null">X</button>
-                <div v-for="item in filteredStores" :key="item.id">
-                    <h1>{{ item.name }}</h1>
-                    <ItemCard :item="item" />
-                </div>
-            </div>
-        </div>
-    </nav>
+        </nav>
+    </div>
 </template>
-
 
 <script>
 import axios from 'axios';
@@ -58,20 +84,36 @@ export default {
     },
     data() {
         return {
+            show: false,
             currentStore: [],
             filteredStores: [],
             stores: [],
             query: '',
-            selectedCategory: '',
-            maxPrice: null,
             minPrice: null,
+            maxPrice: null,
+            currentFilters: {
+                category: [],
+                minPrice: null,
+                maxPrice: null,
+                query: '',
+            },
+            openItemId: null,
         };
     },
     computed: {
-        uniqueCategories() {
+        uniqueCategoriesWithImages() {
             if (this.currentStore.length > 0) {
-                const categories = this.currentStore.map(item => item.category);
-                return [...new Set(categories)]; // Remove duplicates
+                const categories = this.currentStore.reduce((acc, item) => {
+                    if (!acc[item.category]) {
+                        acc[item.category] = item.image;
+                    }
+                    return acc;
+                }, {});
+
+                return Object.entries(categories).map(([name, image]) => ({
+                    name,
+                    image,
+                }));
             }
             return [];
         },
@@ -80,19 +122,41 @@ export default {
         this.fetchStores();
     },
     methods: {
+        openItem(itemId) {
+            this.openItemId = itemId;
+        },
+        setMaxPrice(maxPrice) {
+            this.currentFilters.maxPrice = maxPrice;
+            if (maxPrice === null) this.maxPrice = '';
+        },
+        closeItem() {
+            this.openItemId = null;
+        },
+        setQuery(query) {
+            this.currentFilters.query = query;
+            if (query === '') this.query = '';
+            console.log('Query:', this.query);
+        },
+        setMinPrice(minPrice) {
+            this.currentFilters.minPrice = minPrice;
+            if (minPrice === null) this.minPrice = '';
+            console.log('Min price:', this.minPrice);
+        },
+        setCategory(category) {
+            this.currentFilters.category.push(category);
+        },
+        clearCategory(category) {
+            this.currentFilters.category = this.currentFilters.category.filter(name => name !== category);
+        },
         filterStores() {
-            if (this.currentStore.length > 0) {
-                this.filteredStores = this.currentStore.filter(item => {
-                    const matchesQuery = item.name.toLowerCase().includes(this.query.toLowerCase());
-                    const matchesCategory = this.selectedCategory === '' || item.category === this.selectedCategory;
-                    const matchesMinPrice = this.minPrice === null || item.price >= this.minPrice;
-                    const matchesMaxPrice = this.maxPrice === null || item.price <= this.maxPrice;
-                    
-                    return matchesQuery && matchesCategory && matchesMinPrice && matchesMaxPrice;
-                });
-            } else if (this.query === '') {
-                this.filteredStores = [];
-            }
+            this.filteredStores = this.currentStore.filter(item => {
+                return (
+                    (this.currentFilters.category.length === 0 || this.currentFilters.category.includes(item.category)) &&
+                    (this.currentFilters.minPrice === null || item.price >= this.currentFilters.minPrice) &&
+                    (this.currentFilters.maxPrice === null || item.price <= this.currentFilters.maxPrice) &&
+                    (this.currentFilters.query === '' || item.name.toLowerCase().includes(this.currentFilters.query.toLowerCase()))
+                );
+            });
         },
         setCurrentStore(store) {
             console.log('Current store:', store);
@@ -110,8 +174,10 @@ export default {
             try {
                 const response = await axios.get(`http://localhost:8080/${store}`);
                 this.currentStore = response.data;
+                this.filteredStores = this.currentStore;
+                this.show = true;
                 console.log('Current store:', this.currentStore);
-                this.filterStores(); // Ensure stores are filtered when data is fetched
+                this.filterStores();
             } catch (error) {
                 console.error('Error fetching store data:', error);
             }
@@ -126,6 +192,28 @@ export default {
     padding: 10px;
     margin-bottom: 10px;
     box-sizing: border-box;
+}
+
+.category-container {
+    display: flex;
+    overflow-x: auto;
+    padding: 10px 0;
+}
+
+.category-item {
+    flex: 0 0 auto;
+    margin-right: 10px;
+    text-align: center;
+    cursor: pointer;
+    width: 120px;
+}
+
+.category-item img {
+    width: 100%;
+    height: 120px;
+    object-fit: cover;
+    border-radius: 10px;
+    margin-bottom: 5px;
 }
 
 .suggestions {
